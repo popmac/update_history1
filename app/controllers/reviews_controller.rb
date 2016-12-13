98feb826ac728b2ca8bb2706d0636a5bee714aff
popmac <matsukurahideki@matsukurahideki-no-MacBook-Pro.local>
2016-12-13 17:06:17 +0900

    reviewを作成する機能を実装した。

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,2 +1,17 @@
 class ReviewsController < ApplicationController
+
+  def new
+    @review = Review.new
+  end
+
+  def create
+    Review.create(review_params)
+    redirect_to :root
+  end
+
+
+  private
+  def review_params
+    params.require(:review).permit(:name, :content).merge(user_id: current_user.id)
+  end
 end
