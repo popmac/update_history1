c548a571f58d81cdece0cef70ec4119956f79e61
popmac <matsukurahideki@matsukurahideki-no-MacBook-Pro.local>
2016-12-13 17:37:35 +0900

    'rake db:migrate'を実行した。

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161213073941) do
+ActiveRecord::Schema.define(version: 20161213083324) do
+
+  create_table "comments", force: :cascade do |t|
+    t.text     "content",    limit: 65535
+    t.integer  "review_id",  limit: 4
+    t.integer  "user_id",    limit: 4
+    t.datetime "created_at",               null: false
+    t.datetime "updated_at",               null: false
+  end
 
   create_table "homes", force: :cascade do |t|
     t.datetime "created_at", null: false
