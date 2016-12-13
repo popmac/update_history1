6142d8c7b3f5f4a5a377369ae5016f2f7e1cda79
popmac <matsukurahideki@matsukurahideki-no-MacBook-Pro.local>
2016-12-13 22:05:24 +0900

    commentを作成する機能を実装した。

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,2 +1,19 @@
 class CommentsController < ApplicationController
+
+  def new
+    @review = Review.find(params[:review_id])
+    @comment = Comment.new
+  end
+
+  def create
+    @review = Review.find(params[:review_id])
+    Comment.create(comment_params)
+    redirect_to "/reviews/#{@review.id}"
+  end
+
+  private
+  def comment_params
+    params.require(:comment).permit(:content).merge(user_id: current_user.id, review_id: @review.id)
+  end
+
 end
