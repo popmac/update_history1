bd6714d7c9f5a82b8ce6cf1cbf1749796be9a568
popmac <matsukurahideki@matsukurahideki-no-MacBook-Pro.local>
2016-12-14 00:31:03 +0900

    マイグレーションファイルを編集した。

--- db/migrate/20161213152738_create_likes.rb
+++ db/migrate/20161213152738_create_likes.rb
@@ -1,7 +1,8 @@
 class CreateLikes < ActiveRecord::Migration
   def change
     create_table :likes do |t|
-
+      t.integer :review_id
+      t.integer :user_id
       t.timestamps null: false
     end
   end
