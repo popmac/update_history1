95fad5077db966a038dac398783ed4c73d8e27cb
popmac <matsukurahideki@matsukurahideki-no-MacBook-Pro.local>
2016-12-13 17:15:59 +0900

    reviewを編集する機能を実装した。

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -13,6 +13,16 @@ class ReviewsController < ApplicationController
     @review = Review.find(params[:id])
   end
 
+  def edit
+    @review = Review.find(params[:id])
+  end
+
+  def update
+    review = Review.find(params[:id])
+    review.update(review_params)
+    redirect_to :root
+  end
+
   def destroy
     review = Review.find(params[:id])
     review.destroy
