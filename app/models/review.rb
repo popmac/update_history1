f2101ccd5dd6163c0f2929e247e19fff6a8a093f
popmac <matsukurahideki@matsukurahideki-no-MacBook-Pro.local>
2016-12-13 17:39:46 +0900

    アソシエーションを組んだ。

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,3 +1,4 @@
 class Review < ActiveRecord::Base
   belongs_to :user
+  has_many :comments
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,5 @@ class User < ActiveRecord::Base
          :recoverable, :rememberable, :trackable, :validatable
   validates :username, presence: true, uniqueness: true
   has_many :reviews
+  has_many :comments
 end
