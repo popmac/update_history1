88a6fc5124b0792006e3938a6f85c7b3e9e713a9
popmac <matsukurahideki@matsukurahideki-no-MacBook-Pro.local>
2016-12-14 20:18:27 +0900

    UserモデルとUserRelationshipモデルでアソシエーションを組んだ。

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,4 +7,8 @@ class User < ActiveRecord::Base
   has_many :reviews, dependent: :destroy
   has_many :comments, dependent: :destroy
   has_many :likes, dependent: :destroy
+  has_many :active_relationships, class_name: "UserRelationship", foreign_key: "follower_id", dependent: :destroy
+  has_many :passive_relationships, class_name: "UserRelationship", foreign_key: "followed_id", dependent: :destroy
+  has_many :following, through: :active_relationships, source: :followed
+  has_many :followers, through: :passive_relationships, source: :follower
 end
