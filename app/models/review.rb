37f6a54eea1f5507a5f959fcfcaf453e82f5c182
popmac <matsukurahideki@matsukurahideki-no-MacBook-Pro.local>
2016-12-14 15:49:30 +0900

    Reviewモデルにlikeの判定メソッドを追加した。

--- app/models/review.rb
+++ app/models/review.rb
@@ -2,4 +2,9 @@ class Review < ActiveRecord::Base
   belongs_to :user
   has_many :comments, dependent: :destroy
   has_many :likes, dependent: :destroy
+
+  def like_user(user_id)
+    likes.find_by(user_id: user_id)
+  end
+
 end
