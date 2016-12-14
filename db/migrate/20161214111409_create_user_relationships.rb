4ed30c7a777f1642ec4eba308342894c473e26f4
popmac <matsukurahideki@matsukurahideki-no-MacBook-Pro.local>
2016-12-14 20:15:18 +0900

    マイグレーションファイルを編集した。

--- db/migrate/20161214111409_create_user_relationships.rb
+++ db/migrate/20161214111409_create_user_relationships.rb
@@ -1,8 +1,13 @@
 class CreateUserRelationships < ActiveRecord::Migration
   def change
     create_table :user_relationships do |t|
-
+      t.integer :follower_id
+      t.integer :followed_id
       t.timestamps null: false
     end
+    add_index :user_relationships, :follower_id
+    add_index :user_relationships, :followed_id
+    # follower_idとfollowed_idの組み合わせが一意性を持つようにする
+    add_index :user_relationships, [:follower_id, :followed_id], unique: true
   end
 end
