d6b77ae35666aa272710bd59f35aa8862723cf3c
popmac <matsukurahideki@matsukurahideki-no-MacBook-Pro.local>
2016-12-15 21:25:48 +0900

    ReviewモデルとCheckedReviewモデルでアソシエーションを組んだ。

--- app/models/review.rb
+++ app/models/review.rb
@@ -3,6 +3,7 @@ class Review < ActiveRecord::Base
   has_many :comments, dependent: :destroy
   has_many :likes, dependent: :destroy
   has_paper_trail
+  has_many :checked_reviews
 
   def like_user(user_id)
     likes.find_by(user_id: user_id)
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161215093221) do
+ActiveRecord::Schema.define(version: 20161215121911) do
+
+  create_table "checked_reviews", force: :cascade do |t|
+    t.integer  "review_id",        limit: 4
+    t.integer  "checking_user_id", limit: 4
+    t.boolean  "checked_flag",               default: false, null: false
+    t.datetime "created_at",                                 null: false
+    t.datetime "updated_at",                                 null: false
+  end
 
   create_table "comments", force: :cascade do |t|
     t.text     "content",    limit: 65535
