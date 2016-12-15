3736fc21d0a5325b3b9009e5935a51300ab8fb33
popmac <matsukurahideki@matsukurahideki-no-MacBook-Pro.local>
2016-12-15 22:44:58 +0900

    Commentモデルにcommentが既読かどうか判定するメソッドを追加した。

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,4 +2,8 @@ class Comment < ActiveRecord::Base
   belongs_to :user
   has_paper_trail
   has_many :checked_comments
+
+  def check_unread_comment(user_id, review_id, comment_id)
+    self.checked_comments.where(checking_user_id: user_id, review_id: review_id, comment_id: comment_id)
+  end
 end
