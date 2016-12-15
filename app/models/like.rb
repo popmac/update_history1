a4534549b08e20e4420a9605534a7db1cf21c3c4
popmac <matsukurahideki@matsukurahideki-no-MacBook-Pro.local>
2016-12-15 23:57:41 +0900

    Likeモデルにlikeが既読かどうか判定するメソッドを追加した。

--- app/models/like.rb
+++ app/models/like.rb
@@ -4,4 +4,8 @@ class Like < ActiveRecord::Base
   validates :user_id, :uniqueness => {:scope => :review_id}
   has_paper_trail
   has_many :checked_likes
+
+  def checked_unread_like(user_id, review_id, like_id)
+    self.checked_likes.where(checking_user_id: user_id, review_id: review_id, like_id: like_id)
+  end
 end
