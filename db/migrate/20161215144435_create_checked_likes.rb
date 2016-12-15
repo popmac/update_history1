a534cf436a70143d2834e7104a356a6d0d42ac04
popmac <matsukurahideki@matsukurahideki-no-MacBook-Pro.local>
2016-12-15 23:46:21 +0900

    マイグレーションファイルを編集した。

--- db/migrate/20161215144435_create_checked_likes.rb
+++ db/migrate/20161215144435_create_checked_likes.rb
@@ -1,7 +1,10 @@
 class CreateCheckedLikes < ActiveRecord::Migration
   def change
     create_table :checked_likes do |t|
-
+      t.integer :review_id
+      t.integer :like_id
+      t.integer :checking_user_id
+      t.boolean :checked_flag, default: false, null: false
       t.timestamps null: false
     end
   end
