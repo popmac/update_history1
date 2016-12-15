5bb1b1bd8654faa53a8070be1d1dea08cd6ed580
popmac <matsukurahideki@matsukurahideki-no-MacBook-Pro.local>
2016-12-15 21:23:13 +0900

    マイグレーションファイルを編集した。

--- db/migrate/20161215121911_create_checked_reviews.rb
+++ db/migrate/20161215121911_create_checked_reviews.rb
@@ -1,7 +1,9 @@
 class CreateCheckedReviews < ActiveRecord::Migration
   def change
     create_table :checked_reviews do |t|
-
+      t.integer :review_id
+      t.integer :checking_user_id
+      t.boolean :checked_flag, default: false, null: false
       t.timestamps null: false
     end
   end
